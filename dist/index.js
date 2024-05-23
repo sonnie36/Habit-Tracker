9b2573b898955ff33fad5d4a1b12d9c3f3a6e3ff
sonnie36 <sonniemwati36@gmail.com>
2024-05-23 06:42:51 -0700

    Included add functionality,display function and used REST API to store my data

---
+++ dist/index.js
@@ -0,0 +1,71 @@
+"use strict";
+const add = document.querySelector('.addBtn');
+const details = document.querySelector('.details');
+const cards = document.querySelector('.cardDisplay');
+add.addEventListener('click', () => {
+    while (details.firstChild) {
+        details.removeChild(details.firstChild);
+    }
+    console.log('clicked');
+    const nameLabel = document.createElement('label');
+    nameLabel.textContent = 'Name of habit';
+    nameLabel.classList.add('label');
+    const inputName = document.createElement('input');
+    inputName.type = 'text';
+    inputName.placeholder = 'Habit name';
+    const DateLabel = document.createElement('label');
+    DateLabel.textContent = 'Start date';
+    DateLabel.classList.add('label');
+    const inputDate = document.createElement('input');
+    inputDate.type = 'date';
+    inputDate.placeholder = 'Start date';
+    const Submit = document.createElement('button');
+    Submit.textContent = 'Submit';
+    Submit.classList.add('submitBtn');
+    Submit.addEventListener('click', () => {
+        const habitName = inputName.value;
+        const startDate = inputDate.value;
+        let data = {
+            name: habitName,
+            startDate: startDate
+        };
+        fetch('http://localhost:3000/habits', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json());
+    });
+    details.appendChild(nameLabel);
+    details.appendChild(inputName);
+    details.appendChild(DateLabel);
+    details.appendChild(inputDate);
+    details.appendChild(Submit);
+    //   displayCards();
+});
+// ...
+const displayCards = () => {
+    fetch('http://localhost:3000/habits', {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then((data) => {
+        console.log(data);
+        data.forEach((habit) => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            const habitName = document.createElement('h3');
+            habitName.textContent = habit.name;
+            const startDate = document.createElement('p');
+            startDate.textContent = habit.startDate;
+            card.appendChild(habitName);
+            card.appendChild(startDate);
+            cards.appendChild(card);
+        });
+    })
+        .catch(error => {
+        console.error('Error:', error);
+    });
+};
+displayCards();
